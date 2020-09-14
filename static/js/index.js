1c626ffbfe4b05d2c6e2ecdd03cdde30ba03fab2
silva-cris <70932353+silva-cris@users.noreply.github.com>
2020-09-13 21:55:15 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,42 +1,7 @@
-/*function ACTIVAR() {
-	
-	console.log("ACTIVAR");
-        message = new Paho.MQTT.Message("ACTIVADO");
-        message.destinationName = "menalyluzuriaga@gmail.com/test1";
-        client.send(message);
-		
-		        
-  
-}
-function DESACTIVAR(){	
-	
-	
-	console.log("DESACTIVAR");
-        message = new Paho.MQTT.Message("DESACTIVADO");
-        message.destinationName = "menalyluzuriaga@gmail.com/test1";
-        client.send(message);
-	
-		
-}
-function ENVIAR(){	
-	
-		console.log("ENVIAR");
-		H=document.getElementById("hora").value
-        message = new Paho.MQTT.Message(H);
-        message.destinationName = "menalyluzuriaga@gmail.com/test1";
-        client.send(message);
-		document.getElementById("estado").innerHTML="Activado"
-		
-}*/
-
-
-
-// Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
-  // set callback handlers
+  
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   
@@ -48,12 +13,12 @@ function ENVIAR(){
     onFailure:doFail
   }
 
-  // connect the client
+  
   client.connect(options);
    
-  // called when the client connects
+  
   function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
+    
     console.log("Conectado...");
     client.subscribe("crisfabri24@gmail.com/pagina");
     message = new Paho.MQTT.Message("hola desde la web");
@@ -68,14 +33,14 @@ function ENVIAR(){
 	
   }
 
-  // called when the client loses its connection
+  
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
     }
   }
 
-  // called when a message arrives
+  
   function onMessageArrived(message) {
     console.log("Nuevo mensaje:"+message.payloadString); 
 	document.getElementById("sensor1").innerHTML=message.payloadString.split("=")[1];
